84364f4e832758936036d5c9076e43246c6e7e35
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-09-14 13:15:52 -0500

    fixed px-[%]

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-       <div className="container w-[100%] pt-82 px-6 pb-99 xl:w-1110 xl:min-h-min xl:pt-0 xl:px-0 xl:pb-0">
+       <div className="container w-[100%] pt-82 px-[6.4%] pb-99 xl:w-1110 xl:min-h-min xl:pt-0 xl:px-0 xl:pb-0">
           <main className='flex flex-col gap-49 xl:gap-50'>
             <div className="content_presentation_rated flex flex-col gap-39 xl:flex-row xl:justify-between">
               <Presentation />
