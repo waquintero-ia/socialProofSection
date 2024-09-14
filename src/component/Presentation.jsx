1c371b55df0b80d5759e12dc5b7fe483ef978598
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-09-14 10:08:12 -0500

    role contentinfo, add .json

--- src/component/Presentation.jsx
+++ src/component/Presentation.jsx
@@ -6,7 +6,7 @@ const Presentation = () => {
           <h1 className="text-[40px] font-bold text-purple-900 leading-[80%] tracking-tight xl:text-[56px] xl:leading-[85%]">
             10,000+ of our users love our products.  
           </h1>
-          <p className="text-[19] font-medium text-purple-600 leading-[155%] tracking-wide xl:text-[18px] xl:tracking-normal xl:leading-[140%]">
+          <p className="text-[19] font-medium text-purple-600 leading-[155%] tracking-wide xl:text-[18px] xl:tracking-normal xl:leading-[140%]" role="contentinfo">
             We only provide great products combined with excellent customer service.
             See what our satisfied customers are saying about our services.
           </p>
